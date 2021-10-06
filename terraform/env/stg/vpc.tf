15e37002ebc98629293426ee18dac4d6940e7740
Masashi.Kayahara <masashi.kayahara@arc-connects.com>
2021-10-06 17:43:00 +0900

    Add Stging Network.tf

---
+++ terraform/env/stg/vpc.tf
@@ -0,0 +1,18 @@
+module "vpc" {
+  # see. https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/latest
+  source = "terraform-aws-modules/vpc/aws"
+  version = "3.7.0"
+
+  name = "${var.project_name}-vpc"
+
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-northeast-1a", "ap-northeast-1c", "ap-northeast-1d"]
+  private_subnets = ["10.0.96.0/24", "10.0.128.0/24", "10.0.160.0/24"]
+  public_subnets  = ["10.0.0.0/24", "10.0.32.0/24", "10.0.64.0/24"]
+
+  # 検証用のためNAT G/Wは一つ設定してルーティングさせる( EIPも自動生成する )
+  enable_nat_gateway = true
+  single_nat_gateway  = true
+  tags = var.tags
+}
