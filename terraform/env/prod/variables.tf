38bc524c646fb74e6daff808ad7cf140cf31a5a6
Masashi.Kayahara <masashi.kayahara@arc-connects.com>
2021-10-06 16:33:57 +0900

    Fix var.tags.

--- terraform/env/prod/variables.tf
+++ terraform/env/prod/variables.tf
@@ -14,6 +14,6 @@ variable "tags" {
   default = {
     terraform = "true"
     env = "prod"
-    product = var.project_name
+    product = "multi-account-vpc-peering-sample"
   }
 }
--- terraform/env/stg/variables.tf
+++ terraform/env/stg/variables.tf
@@ -14,6 +14,6 @@ variable "tags" {
   default = {
     terraform = "true"
     env = "stg"
-    product = var.project_name
+    product = "multi-account-vpc-peering-sample"
   }
 }
