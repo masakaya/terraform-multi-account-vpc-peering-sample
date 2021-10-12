6ea63d52706f29d8ebd9be300fa15794975d7aaf
Masashi.Kayahara <masashi.kayahara@arc-connects.com>
2021-10-12 11:20:47 +0900

    Add enable_dns setting.

--- terraform/env/prod/vpc.tf
+++ terraform/env/prod/vpc.tf
@@ -17,5 +17,9 @@ module "vpc" {
   # 検証用のためNAT G/Wは一つ設定してルーティングさせる( EIPも自動生成する )
   enable_nat_gateway = true
   single_nat_gateway  = true
+
+  enable_dns_hostnames = true
+  enable_dns_support = true
+
   tags = var.tags
 }
